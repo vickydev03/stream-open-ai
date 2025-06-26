fd708729575db80a08458d1d151e0556726dbfd1
vickydev03 <vickysinghdev03@gmail.com>
2025-06-26 20:34:15 +0530

    4

--- stream-video.js
+++ stream-video.js
@@ -2,7 +2,7 @@
 import { StreamClient } from "@stream-io/node-sdk";
 import dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY), "ajay";
+console.log(process.env.STREAM_VIDEO_API_KEY,"ajay")
 
 // import { StreamClient } from "@stream-io/node-sdk";
 
@@ -10,8 +10,8 @@ console.log(process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY), "ajay";
 // const { StreamClient } = require("@stream-io/node-sdk");
 // import { StreamVideoClient } from "@stream-io/video-client";
 
-const apiKey = process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY ;
-const secret = process.env.NEXT_PUBLIC_STREAM_VIDEO_API_SECRET;
+const apiKey = process.env.STREAM_VIDEO_API_KEY ;
+const secret = process.env.STREAM_VIDEO_API_SECRET;
 export const streamVideo = new StreamClient(apiKey, secret);
 // export const streamVideoClient = new StreamVideoClient();
 
