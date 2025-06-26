7d305a593ad177bdc3829310258bf23d0386aa2a
vickydev03 <vickysinghdev03@gmail.com>
2025-06-26 20:02:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// const express = require("express");
+import express from "express";
+// import { configDotenv } from "dotenv";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+// const dotenv = require("dotenv");
+// configDotenv()
+// dotenv.config();
+// console.log(process.env.STREAM_VIDEO_API_KEY, "ajay");
+
+import { streamVideo } from "./stream-video.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/connect-openai", async (req, res) => {
+  console.log("Received request to connect OpenAI agent");
+
+  const { agentUserId, instruction, meetingId } = req.body;
+
+  if (!agentUserId || !instruction) {
+    return res
+      .status(400)
+      .json({ error: "agentUserId and instruction are required" });
+  }
+  const call = streamVideo.video.call("default", meetingId);
+
+  try {
+    const client = await streamVideo.video.connectOpenAi({
+      call,
+      openAiApiKey: process.env.OPEN_API_KEY,
+      agentUserId: agentUserId,
+    });
+
+    client.updateSession({
+      instructions: instruction,
+    });
+
+    res.json({ status: 200, message: "OpenAI agent connected successfully" });
+  } catch (err) {
+    console.error("OpenAI agent connection error:", err);
+    res.status(500).json({ error: "Agent connection failed" });
+  }
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
---
+++ stream-video.js
@@ -0,0 +1,17 @@
+
+import { StreamClient } from "@stream-io/node-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY), "ajay";
+
+// import { StreamClient } from "@stream-io/node-sdk";
+
+// or
+// const { StreamClient } = require("@stream-io/node-sdk");
+// import { StreamVideoClient } from "@stream-io/video-client";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY ;
+const secret = process.env.NEXT_PUBLIC_STREAM_VIDEO_API_SECRET;
+export const streamVideo = new StreamClient(apiKey, secret);
+// export const streamVideoClient = new StreamVideoClient();
+
