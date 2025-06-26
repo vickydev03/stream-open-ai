86624f46e7d6af848e9ed2e0f9cbd6198a598f07
vickydev03 <vickysinghdev03@gmail.com>
2025-06-26 20:21:58 +0530

    3

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ app.post("/connect-openai", async (req, res) => {
   }
 });
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
   console.log("Server is running on port 4000");
 });
